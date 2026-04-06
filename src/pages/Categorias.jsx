6de8d9d86dd373d8c2a3055e0859e303a30d7380
Danieldaviddf <danieldaviddf@hotmail.com>
2026-04-06 09:37:42 +0200

    Creación de pvistas

---
+++ src/pages/Categorias.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Categorias = () => {
+  return (
+    <div>Categotias</div>
+  )
+}
+export default Categorias;
---
+++ src/pages/Favoritos.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Favorito = () => {
+  return (
+    <div>Favorito</div>
+  )
+}
+export default Favorito;
