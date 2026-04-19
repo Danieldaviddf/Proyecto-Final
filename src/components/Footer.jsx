a45cae44bae393a1ba6b627db663bb2bc7c7972e
Danieldaviddf <danieldaviddf@hotmail.com>
2026-04-19 11:15:21 +0200

    Actualización de enlaces en el footer y creación de README

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="bg-black/70 text-white/70 py-10 border-t border-white/10">
       <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-6">
         
-        {/* Logo o Nombre */}
+        {/* Logo */}
         <div className="text-xl font-serif tracking-widest text-white">
           VHSFlix <span className="text-xs block text-center md:text-left text-orange-300">DESDE 2026</span>
         </div>
@@ -15,22 +15,28 @@ const Footer = () => {
         {/* Copyright */}
         <div className="text-sm">
           © {new Date().getFullYear()} VHSFlix. Todos los derechos reservados.
+
         </div>
 
-        {/* Redes Sociales */}
-        <div className="flex items-center gap-6">
-          <a href="#" className="hover:text-orange-300 transition-transform hover:scale-125">
-            <FaInstagram size={22} />
-          </a>
-          <a href="#" className="hover:text-orange-300 transition-transform hover:scale-125">
-            <FaFacebook size={22} />
-          </a>
-          <a href="#" className="hover:text-orange-300 transition-transform hover:scale-125">
-            <FaWhatsapp size={22} />
-          </a>
+          {/* Bloque Persona */}
+          <div className="flex flex-col items-center gap-2">
+            <div className="text-sm font-medium">
+              Daniel David Díaz Flores
+            </div>
+            <div className="flex items-center gap-4">
+              <a href="https://instagram.com/danieldaviddf" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <FaInstagram size={22} />
+              </a>
+              <a href="https://wa.me/34643804335?text=Hola%20quiero%20información" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <FaWhatsapp size={22} />
+              </a>
+              <a href="https://github.com/Danieldaviddf" className="hover:text-orange-300 transition-transform hover:scale-125">
+                <i class="text-lg fa-brands fa-github"></i>
+              </a>
+            </div>
+          </div>
         </div>
 
-      </div>
     </footer>
   );
 };
